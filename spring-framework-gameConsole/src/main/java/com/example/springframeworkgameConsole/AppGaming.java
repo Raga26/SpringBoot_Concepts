b00491beb2eb03f2feda2a96ab16a2adc1d016d3
Ragamaliga <mragamaliga@gmail.com>
2024-01-04 17:14:27 +0530

    version 1

--- spring-framework-gameConsole/src/main/java/com/example/springframeworkgameConsole/AppGaming.java
+++ spring-framework-gameConsole/src/main/java/com/example/springframeworkgameConsole/AppGaming.java
@@ -3,8 +3,9 @@ package com.example.springframeworkgameConsole;
 public class AppGaming {
 
     public static void main(String[] args) {
-        var mariogame = new MarioGame();
-        var gameRunner = new GameRunner(mariogame);
+        //var mariogame = new MarioGame();
+        var PacmanGame = new PacmanGame();
+        var gameRunner = new GameRunner(PacmanGame);
         gameRunner.run();
 
     }
