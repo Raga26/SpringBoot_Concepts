7810bebe354c08708777890a6f908b437884b8b0
Ragamaliga <mragamaliga@gmail.com>
2024-01-06 13:22:21 +0530

    version 2

--- spring-framework-gameConsole/src/main/java/com/example/springframeworkgameConsole/MarioGame.java
+++ spring-framework-gameConsole/src/main/java/com/example/springframeworkgameConsole/MarioGame.java
@@ -1,5 +1,10 @@
 package com.example.springframeworkgameConsole;
 
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+@Primary
 public class MarioGame implements GamingConsole{
 
     public void up() {
